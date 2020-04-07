6d8e323c62a43b506e276be4eae6a3fdd3fb0366
Peterson Segatto Müller <p.smuller@outlook.com>
2020-04-07 17:35:29 -0300

    Contador de palavras básico

---
+++ public/js/main.js
@@ -0,0 +1,17 @@
+//contador de palavras do texto principal
+var frase = $(".frase").text();
+var numeroPalavras = frase.split(" ").length;
+var tamanhoFrase = $("#tamanho-frase");
+tamanhoFrase.text(numeroPalavras);
+
+
+//contador de palavras do campo de digitação
+var campo = $(".campo-digitacao");
+campo.on("input", function(){
+  var conteudo = campo.val();
+  var qtdPalavras = conteudo.split(/\S+/).length - 1;
+  $("#contador-palavras").text(qtdPalavras);
+
+  var qtdCaracteres = conteudo.length;
+  $("#contador-caracteres").text(qtdCaracteres);
+});
